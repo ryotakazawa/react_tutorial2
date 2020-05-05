0f022b6a5f5bc5c7d8b774ebbbc36ef785706de6
高沢崚 <ryotakazawaengineer@gmail.com>
2020-05-05 12:44:00 +0900

    Completedgit add .

--- main.jsx
+++ main.jsx
@@ -1,5 +1,7 @@
 function App() {
-  const tweets = [
+  // useStateでツイート配列を取得する
+  // 初期値は今まで通り
+  const [tweets, setTweets] = React.useState([
     {
       id: 0,
       icon: '🌽',
@@ -14,10 +16,15 @@ function App() {
       accountName: 'evidence',
       content: 'かにみそたべたい'
     }
-  ];
+  ]);
+
+  // addTweet関数はuseCallbackで作成する
+  // これも毎回作成していると重くなるので
+  const addTweet = React.useCallback((tweet) => setTweets((prev) => [tweet, ...prev]), [setTweets]);
 
   return (
     <div>
+      <TweetInput addTweet={addTweet}/>
       <Timeline tweets={tweets}/>
     </div>
   );
